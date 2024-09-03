7c157b653ca06be1c6a635d0e730d7e2ea48df5f
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2024-09-03 09:32:13 +0700

    add: mealItem commponent

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,21 +1,15 @@
-export default function MealItem({ meals, onAddCart, isLoading, loadingText }) {
+export default function MealItem({ meal }) {
   return (
-    <>
-      {meals.map((meal) => (
-        <li id={meal.id} className="meal-item">
-          <article>
-            <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
-            <h3>{meal.name}</h3>
-            <p className="meal-item-price">{meal.price}</p>
-            <p className="meal-item-description">{meal.description}</p>
-            <div className="meal-item-actions">
-              <button onClick={() => onAddCart(meal)} className="button">
-                Add to cart
-              </button>
-            </div>
-          </article>
-        </li>
-      ))}
-    </>
+    <li id={meal.id} className="meal-item">
+      <article>
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+        <h3>{meal.name}</h3>
+        <p className="meal-item-price">{meal.price}</p>
+        <p className="meal-item-description">{meal.description}</p>
+        <div className="meal-item-actions">
+          <button className="button">Add to cart</button>
+        </div>
+      </article>
+    </li>
   );
 }
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import MealItem from './MealItem';
 
 export default function Meals() {
   const [loadMeals, setLoadMeals] = useState([]);
@@ -20,7 +21,7 @@ export default function Meals() {
   return (
     <ul id="meals">
       {loadMeals.map((meal) => (
-        <li id={meal.id}>{meal.name}</li>
+        <MealItem meal={meal} />
       ))}
     </ul>
   );
